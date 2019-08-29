970fb394cfb0a88ab5659075f24762f99109f2b6
giovamarr <giovamartin@gmail.com>
2019-08-29 20:58:22 -0300

    primeros servlet

---
+++ TpJavaCine/src/servlet/SalaServlet.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SalaServlet
+ */
+@WebServlet("/SalaServlet")
+public class SalaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SalaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String opcion = request.getParameter("opcion");
+
+			if (opcion.equals("crear")) {
+		System.out.println("crear");
+		RequestDispatcher requestDispatcher= request.getRequestDispatcher("/views/crearSala.jsp");
+			requestDispatcher.forward(request, response);
+			}
+			else if (opcion.equals("modificar"))
+			{System.out.println("modificar");} 
+				else if (opcion.equals("borrar")) 
+				{System.out.println("borrar");}
+			
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
