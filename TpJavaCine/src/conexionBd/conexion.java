09ee956e257a64865e46bfd2376b1356828123d8
giovamarr <giovamartin@gmail.com>
2019-08-29 23:25:36 -0300

    conexion a bd

---
+++ TpJavaCine/src/conexionBd/conexion.java
@@ -0,0 +1,22 @@
+package conexionBd;
+import java.sql.*;
+
+public class conexion {
+
+	public static void main(String[] args) {
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/cine", "root", "lalo123");
+		System.out.println("conexion exitosa");
+		}
+		
+		catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+
+}}
