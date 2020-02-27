30710f8625a44bdec89886eda29bfbe6929854c8
Damian <49252212+DamianBarzola@users.noreply.github.com>
2020-02-27 00:14:46 -0300

    login

--- TpJavaCine/src/servlet/InicioSesion.java
+++ TpJavaCine/src/servlet/InicioSesion.java
@@ -4,6 +4,8 @@ import logic.UsuarioController;
 import entities.Usuario;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,6 +33,22 @@ public class InicioSesion extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    private void response(HttpServletResponse resp, String msg, String link)// para enviar mensajes
+			throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\""
+				+ " \"http://www.w3.org/TR/html4/loose.dtd\">");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; " + "charset=ISO-8859-1\">");
+		out.println("<title>Login result</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<H3>" + msg + "</H3>");// <H2> Texto de prueba (H2)</H2>
+		out.println("</body>");
+		out.println("</html>");
+		out.println("<br><br>");
+		out.println(link);}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
@@ -50,7 +68,7 @@ public class InicioSesion extends HttpServlet {
        
         Usuario usu = usuCon.loginUsuario(email, pass);
   
- 
+ try {
     	if(usu.getEmail()!=null) 
     	{
       
@@ -65,7 +83,7 @@ public class InicioSesion extends HttpServlet {
  		 request.getSession().setAttribute("errorLogin", "Usuario y/o contraseï¿½a incorrecta");	
  		
  		 response.sendRedirect("index.jsp");	
-    	 }	
+    	 }	} catch(Exception e) {response(response,"Usuario o Contraseña Incorrecto","<a href=\"index.jsp\">Volver</a>");}
 	
 }
 	/**
